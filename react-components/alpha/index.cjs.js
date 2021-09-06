405fb95d037b8b6f89a9b75772ae68bf40b72a81
Lingfan Gao <lingfangao@hotmail.com>
2021-09-06 12:46:01 +0200

    use cjs

--- react-components/alpha/index.cjs.js
+++ react-components/alpha/index.cjs.js
@@ -1,3 +1,10 @@
-export const AlphaComponent = () => {
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const AlphaComponent = () => {
   console.log('ALPHA CJS');
 };
+
+exports.AlphaComponent = AlphaComponent;
+
